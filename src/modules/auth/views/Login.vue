97c80684e5e639870309acc5396cc15ca6bbc7c8
Plinio Naves <pliniopjn@hotmail.com>
2019-03-20 14:50:31 -0300

    Login Component: Interface com Vuetify

--- src/modules/auth/views/Login.vue
+++ src/modules/auth/views/Login.vue
@@ -1,5 +1,57 @@
 <template>
-  <v-container>
-    <h1>Login</h1>
+  <v-container fill-height>
+    <v-layout>
+
+      <v-flex>
+        <v-card class="elevation-12">
+
+          <v-toolbar
+            color="primary"
+            dark
+          >
+            <v-toolbar-title>Login</v-toolbar-title>
+          </v-toolbar>
+
+          <v-card-text>
+            <v-form>
+              <v-text-field
+                prepend-icon="email"
+                name="email"
+                label="Email"
+                type="email"
+              ></v-text-field>
+              <v-text-field
+                prepend-icon="lock"
+                name="password"
+                label="Senha"
+                type="password"
+              ></v-text-field>
+            </v-form>
+            <v-btn
+              block
+              depressed
+            >
+              Criar Conta
+            </v-btn>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="primary"
+              large
+            >Login</v-btn>
+          </v-card-actions>
+
+        </v-card>
+      </v-flex>
+
+    </v-layout>
   </v-container>
 </template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
