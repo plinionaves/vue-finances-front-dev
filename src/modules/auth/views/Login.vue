c9eeb7a08a15c0766f11ae39a51625ab02528dcb
Plinio Naves <pliniopjn@hotmail.com>
2019-03-25 18:56:04 -0300

    Login na API GraphQL com AuthService

--- src/modules/auth/views/Login.vue
+++ src/modules/auth/views/Login.vue
@@ -82,6 +82,7 @@
 <script>
 
 import { required, email, minLength } from 'vuelidate/lib/validators'
+import AuthService from './../services/auth-service'
 
 export default {
   name: 'Login',
@@ -154,8 +155,10 @@ export default {
     log () {
       console.log('Vuelidate: ', this.$v)
     },
-    submit () {
+    async submit () {
       console.log('User: ', this.user)
+      const authData = await AuthService.login(this.user)
+      console.log('AuthData: ', authData)
     }
   }
 }
