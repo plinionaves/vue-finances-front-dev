967356f7aa2ec534f921d7da788e73861b3694b0
Plinio Naves <pliniopjn@hotmail.com>
2019-04-10 17:58:33 -0300

    Protegendo rotas que necessitam de autenticação

--- src/modules/dashboard/router/index.js
+++ src/modules/dashboard/router/index.js
@@ -1,5 +1,9 @@
 const Dashboard = () => import('./../views/Dashboard.vue')
 
 export default [
-  { path: '/dashboard', component: Dashboard }
+  {
+    path: '/dashboard',
+    component: Dashboard,
+    meta: { requiresAuth: true }
+  }
 ]
--- src/router.js
+++ src/router.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import { AUTH_TOKEN } from '@/plugins/apollo'
+import AuthService from '@/modules/auth/services/auth-service'
 import authRoutes from '@/modules/auth/router'
 import dashboardRoutes from '@/modules/dashboard/router'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
@@ -15,3 +17,26 @@ export default new Router({
     { path: '', redirect: '/login' }
   ]
 })
+
+router.beforeEach(async (to, from, next) => {
+  if (to.matched.some(route => route.meta.requiresAuth)) {
+    const token = window.localStorage.getItem(AUTH_TOKEN)
+    const loginRoute = {
+      path: '/login',
+      query: { redirect: to.fullPath }
+    }
+    if (token) {
+      try {
+        await AuthService.user({ fetchPolicy: 'network-only' })
+        return next()
+      } catch (error) {
+        console.log('Auto Login Error: ', error)
+        return next(loginRoute)
+      }
+    }
+    return next(loginRoute)
+  }
+  next()
+})
+
+export default router
