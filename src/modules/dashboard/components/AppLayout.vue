912e7eb59f0f0002b26c2914d15f08f938001455
Plinio Naves <pliniopjn@hotmail.com>
2019-04-11 18:01:02 -0300

    Menu para o Dashboard com Vuetify NavigationDrawer

--- src/modules/dashboard/components/AppLayout.vue
+++ src/modules/dashboard/components/AppLayout.vue
@@ -1,15 +1,23 @@
 <template>
-  <AppToolbar />
+  <div>
+    <AppToolbar v-model="drawer" />
+    <AppMenu v-model="drawer" />
+  </div>
 </template>
 
 <script>
 
+import AppMenu from './AppMenu.vue'
 import AppToolbar from './AppToolbar.vue'
 
 export default {
   name: 'AppLayout',
   components: {
+    AppMenu,
     AppToolbar
-  }
+  },
+  data: () => ({
+    drawer: false
+  })
 }
 </script>
---
+++ src/modules/dashboard/components/AppMenu.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-navigation-drawer
+    :value="value"
+    @input="$emit('input', $event)"
+    :mini-variant="mini"
+    absolute
+    temporary
+  >
+
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: 'AppMenu',
+  props: {
+    value: Boolean
+  },
+  data: () => ({
+    items: [
+      { title: 'Home', icon: 'dashboard', url: '/dashboard', exact: true }
+    ],
+    mini: false
+  })
+}
+</script>
