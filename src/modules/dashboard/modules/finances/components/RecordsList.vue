8dcc667c0e1756ab408c94cd4954dcacfd419f0e
Plinio Naves <pliniopjn@hotmail.com>
2019-04-15 14:32:32 -0300

    RecordsList: listando Lançamentos do usuário

---
+++ src/modules/dashboard/modules/finances/components/RecordsList.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-list two-line>
+    <RecordsListItem
+      v-for="record in records"
+      :key="record.id"
+      :record="record"
+    />
+  </v-list>
+</template>
+
+<script>
+
+import RecordsListItem from './RecordsListItem.vue'
+import RecordsService from './../services/records-service'
+
+export default {
+  name: 'RecordsList',
+  components: {
+    RecordsListItem
+  },
+  data: () => ({
+    records: []
+  }),
+  async created () {
+    this.records = await RecordsService.records()
+  }
+}
+</script>
--- src/modules/dashboard/modules/finances/views/RecordsHome.vue
+++ src/modules/dashboard/modules/finances/views/RecordsHome.vue
@@ -1,3 +1,18 @@
 <template>
-  <h1>Lançamentos</h1>
+  <div>
+    <h1>Lançamentos</h1>
+    <RecordsList />
+  </div>
 </template>
+
+<script>
+
+import RecordsList from './../components/RecordsList.vue'
+
+export default {
+  name: 'RecordsHome',
+  components: {
+    RecordsList
+  }
+}
+</script>
