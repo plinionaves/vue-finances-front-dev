5a270190651c777397e771e8dde967d3806a9684
Plinio Naves <pliniopjn@hotmail.com>
2019-04-15 17:09:42 -0300

    Agrupando lista de Lançamentos por data

--- src/modules/dashboard/modules/finances/components/RecordsList.vue
+++ src/modules/dashboard/modules/finances/components/RecordsList.vue
@@ -3,18 +3,25 @@
     two-line
     subheader
   >
-    <v-subheader>05/03/2019</v-subheader>
-    <RecordsListItem
-      v-for="record in records"
-      :key="record.id"
-      :record="record"
-    />
-    <v-divider></v-divider>
+    <template v-for="(records, date, index) in mappedRecords">
+      <v-subheader :key="date">{{ date }}</v-subheader>
+      <RecordsListItem
+        v-for="record in records"
+        :key="record.id"
+        :record="record"
+      />
+      <v-divider
+        :key="`${date}-${index}`"
+        v-if="showDivider(index, mappedRecords)"
+      ></v-divider>
+    </template>
   </v-list>
 </template>
 
 <script>
 
+import moment from 'moment'
+import { groupBy } from '@/utils'
 import RecordsListItem from './RecordsListItem.vue'
 import RecordsService from './../services/records-service'
 
@@ -26,8 +33,20 @@ export default {
   data: () => ({
     records: []
   }),
+  computed: {
+    mappedRecords () {
+      return groupBy(this.records, 'date', (record, dateKey) => {
+        return moment(record[dateKey]).format('DD/MM/YYYY')
+      })
+    }
+  },
   async created () {
     this.records = await RecordsService.records()
+  },
+  methods: {
+    showDivider (index, object) {
+      return index < Object.keys(object).length - 1
+    }
   }
 }
 </script>
