9174ce2dca0a99d3fc18f5050262802806addaf7
Plinio Naves <pliniopjn@hotmail.com>
2019-04-15 13:35:11 -0300

    RecordsListItem para exibir Lançamento

---
+++ src/modules/dashboard/modules/finances/components/RecordsListItem.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-list-tile avatar>
+
+    <v-list-tile-avatar>
+      <v-icon :class="[ recordIconColor(record.type), 'lighten-1 white--text' ]">{{ recordIcon(record.type) }}</v-icon>
+    </v-list-tile-avatar>
+
+    <v-list-tile-content>
+      <v-list-tile-title>{{ record.description }}</v-list-tile-title>
+      <v-list-tile-sub-title>{{ record.category.description }} | {{ record.account.description }}</v-list-tile-sub-title>
+    </v-list-tile-content>
+
+    <v-list-tile-action>
+      <span :class="amountColor(record.amount)">{{ record.amount }}</span>
+    </v-list-tile-action>
+
+  </v-list-tile>
+</template>
+
+<script>
+export default {
+  name: 'RecordsListItem',
+  props: {
+    record: Object
+  },
+  methods: {
+    amountColor (amount) {
+      return amount < 0
+        ? 'error--text text--lighten-1'
+        : 'primary--text text--lighten-1'
+    },
+    recordIcon (type) {
+      return type === 'CREDIT' ? 'arrow_upward' : 'arrow_downward'
+    },
+    recordIconColor (type) {
+      return type === 'CREDIT' ? 'primary' : 'error'
+    }
+  }
+}
+</script>
