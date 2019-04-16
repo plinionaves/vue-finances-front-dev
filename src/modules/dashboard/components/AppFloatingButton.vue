fc294ca07895ec14500dc395fe3314bcc75cd15b
Plinio Naves <pliniopjn@hotmail.com>
2019-04-16 19:41:19 -0300

    Floating Action Button: navegação adicional

---
+++ src/modules/dashboard/components/AppFloatingButton.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-speed-dial
+    v-model="fab"
+    fixed
+    bottom
+    right
+    direction="top"
+    transition="slide-y-reverse-transition"
+  >
+    <template v-slot:activator>
+      <v-btn
+        v-model="fab"
+        color="primary"
+        fab
+      >
+        <v-icon>add</v-icon>
+        <v-icon>close</v-icon>
+      </v-btn>
+    </template>
+    <v-btn
+      fab
+      small
+      color="error"
+      title="Nova Despesa"
+      @click="addRecord('debit')"
+    >
+      <v-icon>remove</v-icon>
+    </v-btn>
+    <v-btn
+      fab
+      small
+      color="primary"
+      title="Nova Receita"
+      @click="addRecord('credit')"
+    >
+      <v-icon>add</v-icon>
+    </v-btn>
+  </v-speed-dial>
+</template>
+
+<script>
+export default {
+  name: 'AppFloatingButton',
+  data: () => ({
+    fab: false
+  }),
+  methods: {
+    addRecord (type) {
+      this.$router.push({
+        name: 'recordsAdd',
+        query: { type }
+      })
+    }
+  }
+}
+</script>
+
+<style lang="styl">
+.v-btn--floating .v-btn__content :not(:only-child):last-child {
+  position: relative;
+}
+</style>
--- src/modules/dashboard/components/AppLayout.vue
+++ src/modules/dashboard/components/AppLayout.vue
@@ -11,17 +11,21 @@
       </v-layout>
     </v-container>
 
+    <AppFloatingButton />
+
   </div>
 </template>
 
 <script>
 
+import AppFloatingButton from './AppFloatingButton.vue'
 import AppMenu from './AppMenu.vue'
 import AppToolbar from './AppToolbar.vue'
 
 export default {
   name: 'AppLayout',
   components: {
+    AppFloatingButton,
     AppMenu,
     AppToolbar
   },
