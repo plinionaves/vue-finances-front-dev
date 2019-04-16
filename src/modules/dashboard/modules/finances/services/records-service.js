e7344702432c8c2f60382867e865e0a8f821b73a
Plinio Naves <pliniopjn@hotmail.com>
2019-04-16 15:36:02 -0300

    Query totalBalance para exibir saldo total atual

--- src/modules/dashboard/modules/finances/services/records-service.js
+++ src/modules/dashboard/modules/finances/services/records-service.js
@@ -1,6 +1,8 @@
 import apollo from '@/plugins/apollo'
+import moment from 'moment'
 
 import RecordsQuery from './../graphql/Records.gql'
+import TotalBalanceQuery from './../graphql/TotalBalance.gql'
 
 const records = async variables => {
   const response = await apollo.query({
@@ -10,6 +12,17 @@ const records = async variables => {
   return response.data.records
 }
 
+const totalBalance = async () => {
+  const response = await apollo.query({
+    query: TotalBalanceQuery,
+    variables: {
+      date: moment().format('YYYY-MM-DD')
+    }
+  })
+  return response.data.totalBalance
+}
+
 export default {
-  records
+  records,
+  totalBalance
 }
