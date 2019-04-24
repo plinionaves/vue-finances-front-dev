8627c7e4c68068a08d05566b1e527dbba4f78a97
Plinio Naves <pliniopjn@hotmail.com>
2019-04-24 15:56:20 -0300

    Adicionando campos iniciais do formulário

--- src/modules/dashboard/modules/finances/views/RecordsAdd.vue
+++ src/modules/dashboard/modules/finances/views/RecordsAdd.vue
@@ -20,8 +20,46 @@
         md8
         lg8
       >
-        <p>Formulário</p>
-        <v-btn @click="log">Log</v-btn>
+        <v-card>
+          <v-card-text>
+            <v-form>
+
+              <v-select
+                name="account"
+                label="Conta"
+                prepend-icon="account_balance"
+              ></v-select>
+
+              <v-select
+                name="category"
+                label="Categoria"
+                prepend-icon="class"
+              ></v-select>
+
+              <v-text-field
+                name="description"
+                label="Descrição"
+                prepend-icon="description"
+                type="text"
+              ></v-text-field>
+
+              <v-text-field
+                name="tags"
+                label="Tags (separadas por vírgula)"
+                prepend-icon="label"
+                type="text"
+              ></v-text-field>
+
+              <v-text-field
+                name="note"
+                label="Observação"
+                prepend-icon="note"
+                type="text"
+              ></v-text-field>
+
+            </v-form>
+          </v-card-text>
+        </v-card>
       </v-flex>
 
     </v-layout>
