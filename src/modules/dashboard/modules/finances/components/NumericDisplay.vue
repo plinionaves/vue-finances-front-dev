9e916bd56970a9123d291d68146d85d67e03d70d
Plinio Naves <pliniopjn@hotmail.com>
2019-04-26 16:05:55 -0300

    NumericDisplay: exibindo teclado numérico

--- src/modules/dashboard/modules/finances/components/NumericDisplay.vue
+++ src/modules/dashboard/modules/finances/components/NumericDisplay.vue
@@ -1,6 +1,42 @@
 <template>
   <v-card :color="color">
-    <div style="height: 345px"></div>
+
+    <v-card-title>
+      <v-spacer></v-spacer>
+      <h3 class="display-2 font-weight-light pt-3">R$ 500,00</h3>
+    </v-card-title>
+
+    <v-card-text>
+      <v-layout
+        row
+        wrap
+        justify-end
+      >
+
+        <v-flex
+          v-for="btn in buttons"
+          :key="btn"
+          xs4
+          pa-1
+        >
+          <v-btn
+            :color="color"
+            class="headline"
+          >{{ btn }}</v-btn>
+        </v-flex>
+
+        <v-flex
+          xs4
+          pa-1
+        >
+          <v-btn icon>
+            <v-icon>backspace</v-icon>
+          </v-btn>
+        </v-flex>
+
+      </v-layout>
+    </v-card-text>
+
   </v-card>
 </template>
 
@@ -9,6 +45,9 @@ export default {
   name: 'NumericDisplay',
   props: {
     color: String
-  }
+  },
+  data: () => ({
+    buttons: [7, 8, 9, 4, 5, 6, 1, 2, 3, 0]
+  })
 }
 </script>
