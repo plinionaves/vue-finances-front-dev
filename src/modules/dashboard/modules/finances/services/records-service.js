f446a7fba7bc7452805228bd475b92647c9777a9
Plinio Naves <pliniopjn@hotmail.com>
2019-05-03 14:58:28 -0300

    Apollo Cache: recalculando Query totalBalance

--- src/modules/dashboard/modules/finances/services/records-service.js
+++ src/modules/dashboard/modules/finances/services/records-service.js
@@ -12,6 +12,7 @@ const createRecord = async variables => {
     mutation: RecordCreateMutation,
     variables,
     update: (proxy, { data: { createRecord } }) => {
+      // records
       const month = moment(createRecord.date.substr(0, 10)).format('MM-YYYY')
       const variables = { month }
 
@@ -30,6 +31,30 @@ const createRecord = async variables => {
         })
       } catch (e) {
         console.log('Query "records" has not been read yet!', e)
+      }
+
+      // totalBalance
+      try {
+        const currentDate = moment().endOf('day')
+        const recordDate = moment(createRecord.date.substr(0, 10))
+        const variables = { date: currentDate.format('YYYY-MM-DD') }
+
+        if (recordDate.isBefore(currentDate)) {
+          const totalBalanceData = proxy.readQuery({
+            query: TotalBalanceQuery,
+            variables
+          })
+
+          totalBalanceData.totalBalance = +(totalBalanceData.totalBalance + createRecord.amount).toFixed(2)
+
+          proxy.writeQuery({
+            query: TotalBalanceQuery,
+            variables,
+            data: totalBalanceData
+          })
+        }
+      } catch (e) {
+        console.log('Query "totalBalance" has not been read yet!', e)
       }
     }
   })
