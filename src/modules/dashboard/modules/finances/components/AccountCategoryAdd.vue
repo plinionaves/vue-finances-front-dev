a5c62c4009c2a58539c6800b90a2ddb202aba063
Plinio Naves <pliniopjn@hotmail.com>
2019-05-03 17:51:11 -0300

    AccountCategoryAdd: validando form com Vuelidate

--- src/modules/dashboard/modules/finances/components/AccountCategoryAdd.vue
+++ src/modules/dashboard/modules/finances/components/AccountCategoryAdd.vue
@@ -1,13 +1,23 @@
 <template>
   <v-card>
-    <v-card-title>{{ title }}</v-card-title>
+    <v-card-title class="headline">{{ title }}</v-card-title>
 
     <v-card-text>
-      <v-text-field label="Descrição"></v-text-field>
+
+      <v-text-field
+        label="Descrição"
+        v-model="$v.item.description.$model"
+      ></v-text-field>
+
       <v-select
         v-if="entity === 'category'"
         label="Operação"
+        v-model="$v.item.operation.$model"
+        :items="operations"
+        item-text="description"
+        item-value="value"
       ></v-select>
+
     </v-card-text>
 
     <v-card-actions>
@@ -19,22 +29,61 @@
       <v-btn
         flat
         color="primary"
+        :disabled="$v.$invalid"
+        @click="save"
       >Salvar</v-btn>
     </v-card-actions>
   </v-card>
 </template>
 
 <script>
+
+import { required, minLength } from 'vuelidate/lib/validators'
+
 export default {
   name: 'AccountCategoryAdd',
   props: {
     entity: String
+  },
+  data: () => ({
+    item: {
+      description: '',
+      operation: ''
+    },
+    operations: [
+      { description: 'Receita', value: 'CREDIT' },
+      { description: 'Despesa', value: 'DEBIT' }
+    ]
+  }),
+  validations () {
+    const validations = {
+      item: {
+        description: {
+          required,
+          minLength: minLength(3)
+        }
+      }
+    }
+
+    if (this.entity === 'account') { return validations }
+
+    return {
+      item: {
+        ...validations.item,
+        operation: { required }
+      }
+    }
   },
   computed: {
     title () {
       return this.entity === 'account'
         ? 'Nova Conta'
         : 'Nova Categoria'
+    }
+  },
+  methods: {
+    save (e) {
+      console.log('Item: ', this.item)
     }
   }
 }
