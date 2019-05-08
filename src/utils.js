242bed4b2262447e0492ae0ff82b2ee7ac24d1be
Plinio Naves <pliniopjn@hotmail.com>
2019-05-08 19:41:41 -0300

    Função para gerar dados dos Gráficos

--- src/utils.js
+++ src/utils.js
@@ -34,11 +34,37 @@ const idx = (object, keyPath) => {
   )
 }
 
+const generateChartData = ({ items, keyToGroup, keyOfValue, aliases, type, backgroundColors }) => {
+  const grouped = groupBy(items, keyToGroup, idx)
+  const response = {}
+
+  for (let key in grouped) {
+    response[(aliases && aliases[key]) || key] =
+      grouped[key].reduce((acc, item) => acc + item[keyOfValue], 0)
+  }
+
+  const labels = Object.keys(response) // Receitas, Despesas
+
+  switch (type) {
+    case 'bar':
+      return {
+        datasets: labels.map((label, index) => ({
+          label: `${label}: ${currencyFormatter().format(response[label])}`,
+          data: [response[label]],
+          backgroundColor: backgroundColors[index],
+          borderWidth: 0
+        }))
+      }
+  }
+}
+
 const generateChartConfigs = (opts) => {
   const { type } = opts
+  const data = generateChartData(opts)
 
   return {
-    type
+    type,
+    data
   }
 }
 
@@ -57,6 +83,7 @@ const registerVuexModule = (rootStore, moduleName, store) => {
 
 export {
   currencyFormatter,
+  generateChartConfigs,
   groupBy,
   errorHandler,
   formatError,
